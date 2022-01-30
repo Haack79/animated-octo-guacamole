653fe19a0c5177647b0742279465733d64e9d6b0
Brian Haack <haack79@hotmail.com>
2022-01-29 20:05:06 -0900

    updated styling with styled component

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router-dom'; 
-
+import styled from 'styled-components';
+const navBar = styled.nav`
+    display: flex;
+    justify-content: flex-start;
+    background-color: #FAEBD7;
+`
 export const NavBar = () => {
     const pages = ['home', 'profile', 'view', 'connect'];
     const navBarLinks = pages.map((page, _, indx) => {
         return (
-            <nav>
-                <Link key={page+indx} to={'/'+page}>{page.toUpperCase}hi</Link>
-            </nav>
+            <navBar>
+                <Link key={page+indx} to={'/'+page}>{page.toUpperCase()+' '}</Link>
+            </navBar>
         )
     });
     return navBarLinks; 
