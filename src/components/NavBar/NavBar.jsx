ece6ddf58ffeca150f34fab515227d879510f988
Brian Haack <haack79@hotmail.com>
2022-01-29 15:38:12 -0900

    added nav bar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router-dom'; 
+
+export const NavBar = () => {
+    const pages = ['/', 'profile', 'view', 'connect'];
+    const navBarLinks = pages.map((page, _, indx) => {
+        return (
+            <nav>
+                <Link key={page+indx} to={'/'+page}>{page.toUpperCase}hi</Link>
+            </nav>
+        )
+    });
+    return navBarLinks; 
+}
