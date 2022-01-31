c78650953ac41d431a8ac38d8cf2856cbaab30b6
Brian Haack <haack79@hotmail.com>
2022-01-30 23:16:18 -0900

    made a display for items

---
+++ src/components/DisplayCard/DisplayCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'; 
+
+const DisplayCard = () => {
+    return (
+        <>
+            <section>
+                <h1>Title</h1>
+                <video></video>
+                <p>text here</p>
+                <p class="comments">comments here</p>
+            </section>
+        </>
+    )
+}
