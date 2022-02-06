e82639a46118be621be5dfc93a924b213751d826
Brian Haack <haack79@hotmail.com>
2022-02-06 00:19:34 -0900

    updating chalk and yargs

--- server/notes.js
+++ server/notes.js
@@ -1,7 +1,20 @@
 // const fs = require('fs');
+// const validator = require('validator');
+const chalk = require('chalk');
+const log = console.log;
+log(chalk.blue('Hello') + 'World' + chalk.red('!')); 
+log(chalk.green('success')); 
+const yellow = chalk.yellow('caution');
+log(yellow); 
 
 const getNotes = () =>  {
     console.log('hello from the notes.js');
 }
+// getting arguments from the terminal cli - argv is an array with all the arguments passed in.
+console.log(process.argv);
 
+
+// checking email and url below with validator
+// console.log(validator.isEmail('brian@e.com')); 
+// console.log(validator.isURL('bleh')); 
 module.exports = getNotes; 
