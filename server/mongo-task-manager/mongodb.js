480da10be111a5b004583d399893d28d92e477d1
Brian Haack <haack79@hotmail.com>
2022-02-21 01:00:51 -0900

    adding mongo connection data

--- server/mongo-task-manager/mongodb.js
+++ server/mongo-task-manager/mongodb.js
@@ -12,5 +12,9 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
     if (error) {
         return console.log('unable to connect to db');
     };
-    console.log('connected correctly'); 
+    const db = client.db(databaseName);
+    db.collection('users').insertOne({
+        name: 'Brian',
+        age: 42
+    })
 });
