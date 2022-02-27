36ea34a0742ffa90694c321738ca7e99b76b1c71
Brian Haack <haack79@hotmail.com>
2022-02-27 00:45:47 -0900

    updated post route api

--- server/postgresql/server/index.js
+++ server/postgresql/server/index.js
@@ -9,10 +9,20 @@ app.use(express.json());
 // routes
 
 // create a todo
-
+app.post('/todos', async(req,res) => {
+    try {
+        const { description } = req.body;
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *",
+        [description])
+    } catch (err) {
+        console.log(err.message); 
+    }
+})
 // get all todo
 
+// get a todo
 
+// delete a todo
 
 app.listen(5000, () => {
     console.log('server has started on 5000');
