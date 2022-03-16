c9ccd45ea839e90ecddff08d33badac8a88c9758
Brian Haack <haack79@hotmail.com>
2022-03-15 22:07:53 -0800

    crated a patch for updating

--- server/mongo-task-manager/mongexpress.js
+++ server/mongo-task-manager/mongexpress.js
@@ -30,43 +30,95 @@ app.post('/tasks', async (req, res) => {
     }
 })
 
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
-        res.status(201).send(users)
-    }).catch((error) => {
-        res.status(500).send(error);
-    })
+app.get('/users', async(req, res) => {
+    try {
+        const users = await User.find({});
+        res.status(201).send(users); 
+    } catch (err) {
+        res.status(500).send(err); 
+    }
+    // User.find({}).then((users) => {
+    //     res.status(201).send(users)
+    // }).catch((error) => {
+    //     res.status(500).send(error);
+    // })
 });
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id
-
-    User.findById(_id).then((user) => {
+    try {
+        const user = await User.findById(_id);
         if (!user) {
-            return res.status(404).send()
+            return res.status(404).send();
         }
-        res.send(user)
-    }).catch((error) => {
-        res.status(500).send(error);
-    })
+        res.send(user);
+    } catch (err) {
+        res.status(500).send(err); 
+    }
+
+    // User.findById(_id).then((user) => {
+    //     if (!user) {
+    //         return res.status(404).send()
+    //     }
+    //     res.send(user)
+    // }).catch((error) => {
+    //     res.status(500).send(error);
+    // })
 })
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = Task.find({});
         res.send(tasks); 
-    }).catch((error) => {
-        res.status(500).send(error); 
-    })
+    } catch (err) {
+        res.status(500).send(err);
+    }
+    // Task.find({}).then((tasks) => {
+    //     res.send(tasks); 
+    // }).catch((error) => {
+    //     res.status(500).send(error); 
+    // })
 });
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id; 
-    Task.findById(_id).then((task) => {
+    try {
+        const task = await Task.findById(_id);
         if (!task) {
             return res.status(404).send();
         }
         res.send(task);
-    }).catch((error) => {
-        res.send(500).send(error);
-    })
+    } catch (err) {
+        res.send(500).send(err); 
+    }
+    // Task.findById(_id).then((task) => {
+    //     if (!task) {
+    //         return res.status(404).send();
+    //     }
+    //     res.send(task);
+    // }).catch((error) => {
+    //     res.send(500).send(error);
+    // })
+})
+// updating
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'email', 'password', 'age'];
+    const isValidOperation = updates.every((updateItem) => allowedUpdates.includes(updateItem)); 
+
+    if (!isValidOperation) {
+        return res.status(400).send({error: 'Invalid updates'})
+    }
+    const _id = req.params.id;
+    try {
+        const user = await User.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true});
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user); 
+
+    } catch (err) {
+        // could be validation or server related issue in this catch
+        res.status(400).send(err); 
+    }
 })
 app.listen(port, () => {
     console.log('server is up on port'+port); 
